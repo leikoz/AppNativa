fcdb97d8ddf21f975b2908ea8c6fd722e83cb246
Samuel <samuel25804@gmail.com>
2021-04-26 16:39:41 -0500

    Se añadieron las imagenes de los botones

--- app/src/main/java/com/example/appnativa/MainActivity.java
+++ app/src/main/java/com/example/appnativa/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.appnativa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 System.out.println("Iniciando juego...");
+                iniciarJuego();
             }
         });
 
@@ -31,5 +33,10 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void iniciarJuego(){
+        Intent i = new Intent(this, Juego.class);
+        startActivity(i);
     }
 }
